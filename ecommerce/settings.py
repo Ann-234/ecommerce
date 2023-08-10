917212363957478b5657368579f04ba53831f936
unknown <hannajames630@gmail.com>
2023-08-10 15:28:50 -0700

    ip added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['44.203.77.19','localhost','*']
 
 
 # Application definition
